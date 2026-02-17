7658410b79fa502c0b5757aa674727a286330b4a
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2026-02-17 18:17:27 +0100

    test: add SymbolsSpec to the test groups

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.Tasty (defaultMain, testGroup)
 
 import qualified Sudoku.GridSpec as GridSpec
+import qualified Sudoku.SymbolsSpec as SymbolsSpec
 import qualified Sudoku.UnitsSpec as UnitsSpec
 
 main :: IO ()
@@ -10,5 +11,6 @@ main =
   defaultMain $
     testGroup "sudoku"
       [ GridSpec.tests
+      , SymbolsSpec.tests
       , UnitsSpec.tests
       ]
